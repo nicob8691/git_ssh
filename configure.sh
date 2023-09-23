ebaca488b817fba966e0803149972ed5f9897b15
nicob8691 <82780760+nicob8691@users.noreply.github.com>
2023-09-23 18:58:59 +0200

    Create configure.sh

---
+++ configure.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+#--- Configuring git
+git config --global user.name "$(whoami)@$(hostname)"
+git config --global user.email "nicob8691@gmail.com"
+git config --global core.editor nano
+git config --global core.fileMode false
