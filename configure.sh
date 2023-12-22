c6237a55509274f34eb9be28a5ae0db5de44317f
nicob8691 <82780760+nicob8691@users.noreply.github.com>
2023-12-22 22:09:56 +0100

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -15,7 +15,6 @@ git config --global core.fileMode false
 ssh-keygen -t ed25519 -C "$(whoami)@$(hostname)"
 
 #--- Prompting public key for adding it to github.com
-#cat ~/.ssh/id_ed25519.pub
 read -p "Make sure you copied pub key to github.com authorised keys. Continue?" -n1 -s
 echo
 
