7746623307a27e45a103dafede0c5372e1683114
nicob8691 <82780760+nicob8691@users.noreply.github.com>
2023-12-22 22:03:10 +0100

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -ex
 
+#--- Configure local repo /home/git
+mkdir -p /home/git
+chmod -R 777 /home/git
+
 #--- Configuring git
 git config --global user.name "$(whoami)@$(hostname)"
 git config --global user.email "nicob8691@gmail.com"
